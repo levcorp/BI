79440f4c8ac266e093951b4fa376e2d09031ddef
Gabriel Angel <angel22586310@gmail.com>
2019-11-06 09:11:35 -0400

    Datos de facturacion QR

--- resources/js/factura.js
+++ resources/js/factura.js
@@ -1,5 +1,19 @@
 import Vue from 'vue';
 import VueQrcodeReader from "vue-qrcode-reader";
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import lang from 'element-ui/lib/locale/lang/es';
+import locale from 'element-ui/lib/locale';
+import Vue2Filters from 'vue2-filters'
+const moment = require('moment');
+require('moment/locale/es');
+
+Vue.use(Vue2Filters)
+Vue.use(require('vue-moment'), {
+    moment
+});
+locale.use(lang);
+Vue.use(ElementUI);
 
 Vue.use(VueQrcodeReader);
 
@@ -8,7 +22,21 @@ new Vue({
     data() {
         return {
             result: '',
-            error: ''   
+            error: '', 
+            factura:{
+                NIT_Emisor:'',
+                Numero_Factura:'',
+                Numero_Autorizacion:'',
+                Fecha_Emision:'',
+                Total:'',
+                Importe_Credito_Fiscal:'',
+                Codigo_Control:'',
+                NIT_Comprador:'',
+                Importe_ICE:'',
+                Importe_Ventas:'',
+                Importe_No_Sujeto:'',
+                Descuentos:''
+            }
         }
     },
     mounted() {
@@ -16,7 +44,19 @@ new Vue({
     },
     methods: {
         onDecode (result) {
-            this.result = result
+            var values = result.split('|');
+            this.factura.NIT_Emisor=values[0];
+            this.factura.Numero_Factura=values[1];
+            this.factura.Numero_Autorizacion=values[2];
+            this.factura.Fecha_Emision=values[3];
+            this.factura.Total=values[4];
+            this.factura.Importe_Credito_Fiscal=values[5];
+            this.factura.Codigo_Control=values[6];
+            this.factura.NIT_Comprador=values[7];
+            this.factura.Importe_ICE=values[8];
+            this.factura.Importe_Ventas=values[9];
+            this.factura.Importe_No_Sujeto=values[10];
+            this.factura.Descuentos=values[11]
         },
         async onInit (promise) {
             try {
--- resources/views/panel/registros/facturas/index.blade.php
+++ resources/views/panel/registros/facturas/index.blade.php
@@ -7,22 +7,198 @@
         <div class="box box-info" style="">
             <div class="box-header">
                 <p style="font-size: 15px">
-                    <strong>&nbsp;&nbsp;</strong>
+                    <strong>&nbsp;&nbsp;Facturas</strong>
                 </p>
             </div>
             <div class="box-body">
                 <div class="row">
-                    <div class="col-sm-12">
-                        <p class="error">@{{ error }}</p>
-                        <p class="decode-result">Last result: <b>@{{ result }}</b></p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-4"></div>
-                    <div class="col-sm-4">
+                    <div class="col-sm-6">
                         <qrcode-stream @decode="onDecode" @init="onInit"/>
+                        <br>
                     </div>
-                    <div class="col-sm-4"></div>
+                    <div class="col-sm-6">
+                        <el-card shadow="always" style="margin-bottom:10px;">
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                            NIT Emisor :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                            @{{factura.NIT_Emisor}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                            Número de Factura :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                            @{{factura.Numero_Factura}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                            Número de Autorización :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                            @{{factura.Numero_Autorizacion}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                                Fecha de emisión :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                            @{{factura.Fecha_Emision}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                                Total :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                                @{{factura.Total}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                            Importe base para el Crédito Fiscal :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                            @{{factura.Importe_Credito_Fiscal}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                            Código de Control :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                            @{{factura.Codigo_Control}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                                NIT / CI / CEX Comprador:
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                                @{{factura.NIT_Comprador}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                                Importe ICE/IEHD/TASAS :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                                @{{factura.Importe_ICE}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                            Importe por ventas no Gravadas o Gravadas :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                             @{{factura.Importe_Ventas}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                            Importe no Sujeto a Crédito Fiscal :
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                            @{{factura.Importe_No_Sujeto}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                            <div style="border-bottom: #909399 solid 1px"></div>
+                            <div class="row" style="margin:5px;">
+                                <div class="col-sm-12">
+                                    <div class="pull-left">
+                                        <strong class="pull-left" style="font-size:12px;color:#606266;">
+                                                Descuentos/Bonificaciones:
+                                        </strong>
+                                    </div>
+                                    <div class="pull-right">
+                                        <strong class="pull-right" style="font-size:12px;color:black">
+                                            @{{factura.Descuentos}}
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </div>
                 </div>
             </div>
         </div>
