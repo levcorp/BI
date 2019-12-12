3c4f87d4b8e90030239dbdfb92300eab2d5e1861
Gabriel Angel <angel22586310@gmail.com>
2019-12-12 16:32:57 -0400

    Arreglo de errores Almacenes

--- app/Hana/SQL/Almacen.php
+++ app/Hana/SQL/Almacen.php
@@ -23,20 +23,20 @@ class Almacen extends ODBC{
         break;
     }
   }
-  public function ArticulosStock($sucursal){   $sql= <<<EOF
-      SELECT T1."U_UbicFis", T0."ItemCode", T0."U_Cod_Vent", T1."OnHand", T0."ItemName", T2."FirmName", T1."WhsCode",
-      CASE WHEN T0."QryGroup1" = 'y' THEN 'Primario'
-      WHEN T0."QryGroup2" = 'Y' THEN 'Campaña'
-      WHEN T0."QryGroup3" = 'Y' THEN 'Campaña'
-      ELSE 'Sin Clasificación' END "Tipo"
-      FROM LEVCORP.OITM T0 INNER JOIN LEVCORP.OITW T1 ON T0."ItemCode" = T1."ItemCode" INNER JOIN LEVCORP.OMRC T2 ON T0."FirmCode" = T2."FirmCode"
-      WHERE  T1."WhsCode" = '$sucursal'
-      AND SUBSTRING(T0."ItemCode",0,5) NOT IN ('401-', '150-')
-      AND T1."OnHand" > 0
+  public function ArticulosStock($sucursal){   
+    $sucursal=$this->WhsCode($sucursal);
+      $sql= <<<EOF
+        SELECT T1."U_UbicFis", T0."ItemCode", T0."U_Cod_Vent", T1."OnHand", T0."ItemName", T2."FirmName", T1."WhsCode",
+        CASE WHEN T0."QryGroup1" = 'y' THEN 'Primario'
+        WHEN T0."QryGroup2" = 'Y' THEN 'Campaña'
+        WHEN T0."QryGroup3" = 'Y' THEN 'Campaña'
+        ELSE 'Sin Clasificación' END "Tipo"
+        FROM LEVCORP.OITM T0 INNER JOIN LEVCORP.OITW T1 ON T0."ItemCode" = T1."ItemCode" INNER JOIN LEVCORP.OMRC T2 ON T0."FirmCode" = T2."FirmCode"
+        WHERE  T1."WhsCode" = '$sucursal'
+        AND SUBSTRING(T0."ItemCode",0,5) NOT IN ('401-', '150-')
+        AND T1."OnHand" > 0
       EOF;
       return parent::query(utf8_decode($sql));
-    $sucursal=$this->WhsCode($sucursal);
- 
   }
   public function EditFabricantes($request){
     $asignaciones=AsignacionAlmacen::select('id')->where('LISTA_ID',$request->lista_id)->get();
